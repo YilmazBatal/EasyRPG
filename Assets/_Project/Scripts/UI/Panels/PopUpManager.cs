0ff59e3f87cf895a5c28054ba2d19836cbe08548
YilmazBatal <b.ogulbey@gmail.com>
2026-04-23 15:06:13 +0300

    Added Animation to pop up manager

--- Assets/_Project/Scripts/UI/Panels/PopUpManager.cs
+++ Assets/_Project/Scripts/UI/Panels/PopUpManager.cs
@@ -11,7 +11,18 @@ public class PopUpManager : MonoBehaviour
 
     public void Close()
     {
+        LeanTween.value(UIManager.Instance.raycastBlocker1.gameObject, 0.5f, 0f, 0.2f).setEaseInOutCubic().setOnUpdate((float val) =>
+        {
+            UIManager.Instance.raycastBlocker1.color = new Color(0f, 0f, 0f, val);
+        });
         UIManager.Instance.raycastBlocker1.gameObject.SetActive(false);
-        Destroy(this.gameObject);
+
+        LeanTween.value(this.gameObject, 1f, 0f, 0.3f).setEaseInOutCubic().setOnUpdate((float val) =>
+        {
+            this.transform.localScale = new Vector3(val, val, val);
+        }).setOnComplete(() => {
+           Destroy(this.gameObject);
+
+        });
     }
 }
